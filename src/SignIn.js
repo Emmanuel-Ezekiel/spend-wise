3a40286040b4f1dd0b6df9f275068f2b8ba19e23
Emmanuel-EEzekiel <emamex44@gmail.com>
2021-12-24 19:25:23 +0100

    add file

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,10 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { BsArrowRight } from 'react-icons/bs'
 
+
+const eye = <FontAwesomeIcon icon={faEye} />;
+
+const arrow = < BsArrowRight />
 function SignIn() {
+
+    const [passwordShown, setPasswordShown] = useState(false);
+    const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
     return (
-        <div>
-            
+        <>
+        <div className="header-signin">
+                {/* <div className="position"></div> */}
+        <p> <b>Monitor</b> user interactions on <b> Spendwise </b></p>
+        <div className="form-sigin">
+            <form action="" name="signin" className='signin-container'>
+                <div className="form-content">
+                    <label htmlFor="">Username</label>
+                    <input type="text" name="username" />
+                </div>
+                <div className="form-content">
+                    <label htmlFor="">Password</label>
+                    <input type={passwordShown ? "text" : "password"} name="password"  />
+                    <span className="show" onClick={togglePasswordVisiblity}> Show </span>
+                </div>
+         </form>
+                <div className="signin-btn">
+                <button className="btn-sign"> Login In </button><span className="arrow-right">{arrow}</span>
+            </div>
+            <p className="signin-account">
+                Don't have an account?
+                <a className="login"href="/SignUp"> Signup</a>
+            </p>
         </div>
+         
+        </div>
+        </>
     )
 }
 
--- src/SignUp.js
+++ src/SignUp.js
@@ -1,26 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BsArrowRight } from 'react-icons/bs'
+import { AiFillEyeInvisible } from 'react-icons/ai'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
-function SignUp() {
+const eye = <FontAwesomeIcon icon={faEye} />;
+
+const arrow = < BsArrowRight />
+
+
+function SignUp() { 
+    const [passwordShown, setPasswordShown] = useState(false);
+    const togglePasswordVisiblity = () => {
+    setPasswordShown(passwordShown ? false : true);
+  };
+
+
     return (
         <>
             <div className="header">
-                <h1> Join the Spendwise</h1>
-            <div className="form">
-                 <form action="">
-                <label htmlFor="">Username</label>
-                <input type="text" />
-                <label htmlFor="">Email</label>
-                <input type="text" />
-                <label htmlFor="">Phone number</label>
-                <input type="text" />
-                <label htmlFor="">Pasword</label>
-                <input type="text" />
-                <label htmlFor="">Confirm password</label>
-                <input type="text" />
-                <label htmlFor="">Referral Code(optional)</label>
-                <input type="text" />
+                    <p className="header-name"> <b>Join</b> the <b>Spendwise</b> </p>
+            <div className="container">
+                <form action="" name="signup" className='form-container'>
+                <div className="form-content">
+                    <label htmlFor="">Username</label>
+                    <input type="text" name="username" />
+                </div>
+                <div className="form-content">
+                    <label htmlFor="">Email</label>
+                    <input type="text" name="Email"  />
+                </div>
+                <div className="form-content">
+                    <label htmlFor="">Phone number</label>
+                    <input type="text" name="phone" />
+                </div>
+                <div className="form-content">
+                    <label htmlFor="">Password</label>
+                    <input type={passwordShown ? "text" : "password"} name="password"  />
+                    <i className="eye" onClick={togglePasswordVisiblity}>{eye}</i>
+                </div>
+                <div className="form-content">
+                    <label htmlFor="">Confirm password</label>
+                    <input 
+                    type={passwordShown ? "text" : "password"}  name="password"
+                    />
+                    <i className="eye" onClick={togglePasswordVisiblity}>{eye}</i>
+                </div>
+                <div className="form-content">
+                    <label htmlFor="">Referral Code (optional)</label>
+                    <input type="text" name="code" />
+                </div>
             </form>
-            <button> Sign Up <span></span></button>
+            <div className="btn">
+                <button> Sign Up </button><span className="arrow-right">{arrow}</span>
+            </div>
+            <p className="sign-account">
+                Already have an account?
+                <a className="login"href="/SignIn"> Login</a>
+            </p>
             </div>
             </div>
         </>
